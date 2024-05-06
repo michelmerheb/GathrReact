81fc3ee8f9452d19def26f2616d76243f45b7389
Michel Merheb <merhebmichel980@gmail.com>
2024-05-06 17:12:46 +0300

    Removed unused libraries

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <App />
     </Provider>
   </React.StrictMode>
-
-)
+);
